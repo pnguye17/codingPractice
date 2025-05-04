a22cd8505243c91172f18edb5ed0b73a3ff2a4ee
pnguye17 <peter.nguyen17@gmail.com>
2025-05-03 23:55:06 -0700

    splitted the strings into array and loop twice

--- javascriptCodePratice/leetcode/rmelement.js
+++ javascriptCodePratice/leetcode/rmelement.js
@@ -34,6 +34,20 @@ var mySqrt = function(x) {
  */
 
 var longestCommonPrefix = function(strs) {
-    //compare each string value and iterate the first letters
-    
+    //split each string in the array in substrings to iterate
+    let prefixContainer = []
+    let prefix = ""
+    for (let i = 0; i < strs.length; i++){
+        for (let j = 1; j < strs.length; j++){
+            let initialWord = strs[i].split('').shift()
+            let compareWord = strs[j].split('').shift()
+            if (initialWord === compareWord){
+                prefixContainer.push(initialWord)
+                initialWord = ''
+                compareWord = ''
+            }
+        }
+    }
+    prefix = prefixContainer.join('')
+    console.log(prefix)
 };
