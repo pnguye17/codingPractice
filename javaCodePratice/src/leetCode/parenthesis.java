33cefac4c55096d446bda75723001b1b0eacfc18
pnguye17 <peter.nguyen17@gmail.com>
2025-05-06 00:34:45 -0700

    starting parenthesis valid problem

---
+++ javaCodePratice/src/leetCode/parenthesis.java
@@ -0,0 +1,35 @@
+package leetCode;
+
+public class parenthesis {
+
+    public static void main(String[] args) {
+        System.out.println(isvalid("Hello"));
+    }
+
+
+    static boolean isvalid(String string){
+
+        // check if the string contains brackers [] {} ()
+        // split the string into array of string and iterate each char for the above
+
+
+        char openBracket = '[';
+        String closeBracket = "]";
+        char openCurly = '{';
+        char closeCurly = '}';
+        char openParenthesis = '(';
+        char closeParentesis = ')';
+    
+        //loop the first char
+
+        for (int i = 0 ; i <= string.length(); i++){
+            if (String.valueOf(i+1).equals(closeBracket)){
+                return true;
+            }
+        }
+
+        return true;
+    }
+}
+
+
